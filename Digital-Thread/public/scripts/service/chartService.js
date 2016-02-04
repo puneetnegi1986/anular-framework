70a94c659be7b4a019980948449e16adac3068a9
puneet negi <puneetnegi1986@gmail.com>
2016-02-04 12:00:08 +0530

    Create chartService.js

---
+++ Digital-Thread/public/scripts/service/chartService.js
@@ -0,0 +1,10 @@
+/*global define */
+define(['angular', 'services-module'], function (angular, services) {
+	'use strict';
+	services.factory('chartService',['utilService','jquery','charts',function(utilService,$,charts){
+		return{
+			
+		}
+	}]);
+	return services;
+});
