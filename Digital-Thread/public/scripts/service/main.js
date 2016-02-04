2729d03defc77d8c2e452afdd6bd3d26c16367f2
puneet negi <puneetnegi1986@gmail.com>
2016-02-04 12:01:26 +0530

    Create main.js

---
+++ Digital-Thread/public/scripts/service/main.js
@@ -0,0 +1,6 @@
+/*
+ * load angular app.services files - angular.module("app.services");
+ */
+define(['./sample-service','./cookieService','./utilityService','./chartService'], function () {
+
+});
